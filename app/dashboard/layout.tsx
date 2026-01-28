cfa4d6310b347ff7bb5cec4c394d35c8f3a122a5
PlutonusDev <plutonus.stream@gmail.com>
2026-01-29 08:11:08 +1000

    Minor style change

--- app/dashboard/layout.tsx
+++ app/dashboard/layout.tsx
@@ -17,7 +17,7 @@ export default ({ children }: { children: React.ReactNode }) => {
     return (
         <UserProvider>
             <HubLayout>
-              <div className="absolute top-0 inset-0 bg-gradient-to-b from-blue-400/20 dark:from-blue-900/20 to-transparent pointer-events-none" />
+              <div className="hidden dark:absolute top-0 inset-0 bg-gradient-to-b from-blue-400/20 dark:from-blue-900/20 to-transparent pointer-events-none" />
               {children}
             </HubLayout>
         </UserProvider>
